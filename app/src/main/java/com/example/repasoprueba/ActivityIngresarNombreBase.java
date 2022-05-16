55054a6a1cc1949b1295674581880abdf665f1e7
Miguel Enrique Bonilla Quishpe <64868918+MiguelBonilla814@users.noreply.github.com>
2022-05-16 12:46:03 -0500

    commit 2 - Activities creadas

---
+++ app/src/main/java/com/example/repasoprueba/ActivityIngresarNombreBase.java
@@ -0,0 +1,14 @@
+package com.example.repasoprueba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityIngresarNombreBase extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ingresar_nombre_base);
+    }
+}
