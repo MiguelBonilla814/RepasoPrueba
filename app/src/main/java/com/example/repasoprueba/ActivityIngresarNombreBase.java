a2ed3e809ca6678fa290536179a2fc29f4a8e766
Miguel Enrique Bonilla Quishpe <64868918+MiguelBonilla814@users.noreply.github.com>
2022-05-16 16:35:04 -0500

    commit 3 - final

--- app/src/main/java/com/example/repasoprueba/ActivityIngresarNombreBase.java
+++ app/src/main/java/com/example/repasoprueba/ActivityIngresarNombreBase.java
@@ -2,13 +2,88 @@ package com.example.repasoprueba;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ActivityIngresarNombreBase extends AppCompatActivity {
+
+    private int LAUNCH_ACTIVITY_3 = 1;
+    private EditText edit_nombre;
+    private EditText edit_base;
+    private Button button_cerrar_Activity2;
+
+    String nombre = "", apellido, base = "", exponente, numero;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingresar_nombre_base);
+
+        edit_nombre = findViewById(R.id.editText_nombre_activity2);
+        edit_base = findViewById(R.id.editText_base_Activity2);
+        button_cerrar_Activity2 = findViewById(R.id.button_cerrar_Activity2);
+
+        if(nombre.equals(""))
+            button_cerrar_Activity2.setEnabled(false);
+
     }
+
+    public void AbrirActivity3(View view){
+        Intent intent = new Intent(this, FinalActivity.class);
+        intent.putExtra("nombre", edit_nombre.getText().toString());
+        intent.putExtra("base", edit_base.getText().toString());
+        startActivityForResult(intent, LAUNCH_ACTIVITY_3);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == LAUNCH_ACTIVITY_3){
+            if(resultCode == Activity.RESULT_OK){
+                nombre = data.getStringExtra("nombre");
+                apellido = data.getStringExtra("apellido");
+                base = data.getStringExtra("base");
+                exponente = data.getStringExtra("exponente");
+                numero = data.getStringExtra("numero");
+
+                /*
+                Toast.makeText(this, nombre, Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, apellido, Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, base, Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, exponente, Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, numero, Toast.LENGTH_SHORT).show();*/
+
+                if(!nombre.equals(""))
+                    button_cerrar_Activity2.setEnabled(true);
+
+            }
+
+            if(resultCode == Activity.RESULT_CANCELED){
+
+            }
+        }
+    }
+
+
+    public void CerrarActivity2(View view){
+        Intent intent = new Intent();
+        intent.putExtra("nombre", nombre);
+        intent.putExtra("apellido", apellido);
+        intent.putExtra("base", base);
+        intent.putExtra("exponente", exponente);
+        intent.putExtra("numero", numero);
+
+        setResult(Activity.RESULT_OK, intent);
+
+        this.finish();;
+    }
+
+
+
 }
--- app/src/main/java/com/example/repasoprueba/FinalActivity.java
+++ app/src/main/java/com/example/repasoprueba/FinalActivity.java
@@ -2,13 +2,58 @@ package com.example.repasoprueba;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class FinalActivity extends AppCompatActivity {
+
+    private TextView text_nombre;
+    private TextView text_base;
+
+    private EditText edit_apellido;
+    private EditText edit_exponente;
+    private EditText edit_numero;
+
+    String nombre, base;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_final);
+
+        text_nombre = findViewById(R.id.editText_nombre_activity3);
+        text_base = findViewById(R.id.editText_base_activity3);
+        edit_apellido = findViewById(R.id.editText_apellido_activity3);
+        edit_exponente = findViewById(R.id.editText_exponente_activity3);
+        edit_numero = findViewById(R.id.editText_factorial_activity3);
+
+        nombre = getIntent().getExtras().getString("nombre");
+        base = getIntent().getExtras().getString("base");
+
+        text_nombre.setText(nombre);
+        text_base.setText(base);
+
+    }
+
+    public void CerrarActivity3(View view){
+        String apellido, exponente, numero;
+
+        apellido = edit_apellido.getText().toString();
+        exponente = edit_exponente.getText().toString();
+        numero = edit_numero.getText().toString();
+
+        Intent intent = new Intent();
+        intent.putExtra("nombre", nombre);
+        intent.putExtra("apellido", apellido);
+        intent.putExtra("base", base);
+        intent.putExtra("exponente", exponente);
+        intent.putExtra("numero", numero);
+
+        setResult(Activity.RESULT_OK, intent);
+        this.finish();
     }
 }
